3d1fcf60bfa31a65e4b94da68cf73d79d2262bdc
vinicio Barbosa do nascimento <viniieg@icloud.com>
2024-10-04 21:51:07 -0300

    Update ex12.py

---
+++ ex12.py
@@ -0,0 +1,4 @@
+base = float(input(" Informe o valor da base do seu triangulo: "))
+altura = float(input("Informe o valor da altura do seu triangulo: "))
+resul = base * altura
+print("A área do seu triangulo é: ")
