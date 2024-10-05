deddff9587466f7445b9abec6d3ea889e15b7c79
vinicio Barbosa do nascimento <viniieg@icloud.com>
2024-10-04 21:46:37 -0300

    Add files via upload

---
+++ ex14.py
@@ -0,0 +1,3 @@
+metro = int(input("Informe o valor do metro para conversão em centimetros:"))
+resul = metro * 100
+print (metro,"metro(s), equivale a: ",resul,"cm.")
---
+++ ex16.py
@@ -0,0 +1,4 @@
+valor = float(input("informe o valor total do produto: R$"))
+desconto = int(input("informe quantos porcento de desconto deseja dar: "))
+resul = valor %% 
+print("O valor do produto com desconto é: R$",resul)
